40ddc3af3caeb211f83a4e7a6471a44cedb5a298
k21971 <keith.simpson1971@gmail.com>
2022-05-27 01:11:25 +0000

    Update install script/hints file for new version (7.1.0).

--- install-to-chroot.sh
+++ install-to-chroot.sh
@@ -22,9 +22,9 @@ COMPRESSBIN="/bin/gzip"
 NH_GIT="/home/build/xNetHack"
 NH_BRANCH="master"
 # HACKDIR from include/config.h; aka nethack subdir inside chroot
-NHSUBDIR="xnethack-7.0.0.1"
+NHSUBDIR="xnethack-7.1.0"
 # VAR_PLAYGROUND from include/unixconf.h
-NH_VAR_PLAYGROUND="/xnethack-7.0.0.1/var/"
+NH_VAR_PLAYGROUND="/xnethack-7.1.0/var/"
 # only define this if dgl was configured with --enable-sqlite
 SQLITE_DBFILE="/dgldir/dgamelaunch.db"
 # END OF CONFIG
@@ -51,8 +51,8 @@ set -e
 umask 022
 
 echo "Creating inprogress and extrainfo directories"
-mkdir -p "$NAO_CHROOT/dgldir/inprogress-xnh7001"
-chown "$USRGRP" "$NAO_CHROOT/dgldir/inprogress-xnh7001"
+mkdir -p "$NAO_CHROOT/dgldir/inprogress-xnh710"
+chown "$USRGRP" "$NAO_CHROOT/dgldir/inprogress-xnh710"
 mkdir -p "$NAO_CHROOT/dgldir/extrainfo-xnh"
 chown "$USRGRP" "$NAO_CHROOT/dgldir/extrainfo-xnh"
 
